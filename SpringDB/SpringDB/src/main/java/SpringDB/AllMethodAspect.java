eade8b670e67cf70fd5e512111c3520243109b5f
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2024-02-19 23:12:20 +0700

    Log in file

--- SpringDB/SpringDB/src/main/java/SpringDB/AllMethodAspect.java
+++ SpringDB/SpringDB/src/main/java/SpringDB/AllMethodAspect.java
@@ -5,11 +5,21 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 @Aspect
 @Component
 public class AllMethodAspect {
     @Before("execution(* SpringDB.PerformerService.*(..))")
     public void logAllMethods(JoinPoint joinPoint){
-        System.out.println("Вариант логирования execution. Был вызван метод " + joinPoint.getSignature().toString());
+//        System.out.println("Вариант логирования execution. Был вызван метод " + joinPoint.getSignature().toString());
+        try{
+            FileWriter fileWriter = new FileWriter("logByExecution.txt", true);
+            fileWriter.write("Method " + joinPoint.getSignature().getName() + " was called\n");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- SpringDB/SpringDB/src/main/java/SpringDB/LoggingAspect.java
+++ SpringDB/SpringDB/src/main/java/SpringDB/LoggingAspect.java
@@ -4,11 +4,22 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 @Aspect
 @Component
 public class LoggingAspect {
     @After("@annotation(SpringDB.TrackUserAction)")
     public void logBeforeMethodCall(JoinPoint joinPoint) {
-        System.out.println("Вариант логирования через annotation. Method " + joinPoint.getSignature().getName() + " was called");
+//        System.out.println("Вариант логирования через annotation. Method " + joinPoint.getSignature().getName() + " was called");
+        try{
+            FileWriter fileWriter = new FileWriter("logByAnnotation.txt", true);
+            fileWriter.write("Method " + joinPoint.getSignature().getName() + " was called\n");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
